b92708b909ede59db691b80dbf4c29e516758423
amitkaintura33-code <amitkaintura33@gmail.com>
2026-08-30 01:50:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-8747352245:AAEOXKZWVidjut5zWfgyXaJJSrnAruYKjUo
+import telebot
+
+TOKEN = "8747352245:AAEOXKZWVidjut5zWfgyXaJJSrnAruYKjUo"
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=["start", "help"])
+def send_welcome(message):
+  bot.reply_to(message, "Hello! Main 24/7 active hun.")
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+  bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
